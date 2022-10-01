aff927e0a7504485505226c871336b8a882818ab
dancefreak97 <decenng@gmail.com>
2022-10-02 03:55:02 +0800

    Calling it Quits

--- Lab3Tutorial/Lab3Tutorial/main.cpp
+++ Lab3Tutorial/Lab3Tutorial/main.cpp
@@ -5,8 +5,19 @@ int main(int argc, char* argv[])
 {
     QApplication app(argc, argv);
 
+    //Hello World button
     QPushButton hello("Hello world!");
-
     hello.show();
+
+    //Calling it Quits
+    QPushButton quit("Quit", 0);
+    quit.resize(75, 30);
+    quit.setFont(QFont("Times", 18, QFont::Bold));
+    QObject::connect(&quit, SIGNAL(clicked()), &app, SLOT(quit()));
+    quit.show();
+
+
+
+
     return app.exec();
 }
