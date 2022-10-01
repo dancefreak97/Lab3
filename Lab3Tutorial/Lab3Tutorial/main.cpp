c1a21b9ebcd316d8614c7255a6b5fdbe84bf3f4c
dancefreak97 <decenng@gmail.com>
2022-10-02 04:08:20 +0800

    Family Values

--- Lab3Tutorial/Lab3Tutorial/main.cpp
+++ Lab3Tutorial/Lab3Tutorial/main.cpp
@@ -1,22 +1,31 @@
 #include <QApplication>
 #include <QPushButton>
+#include <QFont>
+#include <QPushButton>
+#include <QWidget>
 
 int main(int argc, char* argv[])
 {
     QApplication app(argc, argv);
 
-    //Hello World button
-    QPushButton hello("Hello world!");
-    hello.show();
+    //Family Values: part1
+    QWidget window;
+    window.resize(200, 120);
 
-    //Calling it Quits
-    QPushButton quit("Quit", 0);
+    
+    //Hello World -edited for Family Values
+    QPushButton hello("Hello world!", &window);
+    //hello.show();
+    
+    //Calling it Quits -edited for Family Values
+    QPushButton quit("Quit", &window);
     quit.resize(75, 30);
     quit.setFont(QFont("Times", 18, QFont::Bold));
+    quit.setGeometry(10, 40, 180, 40);
     QObject::connect(&quit, SIGNAL(clicked()), &app, SLOT(quit()));
-    quit.show();
-
-
+   
+    //Family Values 2: part 3
+    window.show();
 
 
     return app.exec();
